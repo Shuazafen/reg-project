f0bad3b737d62769cac59e3d97b7fbd12d31e88f
shuazafen <olomoshuaomozafen@gmail.com>
2024-08-27 18:47:36 +0100

    t

--- server/app/views.py
+++ server/app/views.py
@@ -6,11 +6,15 @@ def home(request):
     form = People.objects.all()
     if request.method == 'POST':
         name = request.POST['name']
-        email = request.POST['email']
-        phone = request.POST['phone-number']
-        message = request.POST['message']
-        contact_info = contact(name=name, phone=phone, message=message)
-        contact_info.save()
+        phone = request.POST['phone']
+        birth = request.POST['birth']
+        member_rccg = request.POST['member_rccg']
+        whatsapp_no = request.POST['whatsapp_no']
+        referee = request.POST['referee']
+        attended_prev = request.POST['attended_prev']
+        suggestions = request.POST['suggestions']
+        form_info = People(name=name, phone=phone, birth=birth, member_rccg=member_rccg, whatsapp_no=whatsapp_no, referee=referee, attended_prev=attended_prev, suggestions=suggestions)
+        form_info.save()
     else:
         print('Not sent')
     return render(request, 'index.html',{'form':form})
